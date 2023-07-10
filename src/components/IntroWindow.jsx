eba8d60cd8eab116bac675233d47d6d48112cc0f
P1nkyyyy <pinkavamichal10@gmail.com>
2023-07-10 15:41:25 +0200

    Intro section - animations

--- src/components/IntroWindow.jsx
+++ src/components/IntroWindow.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 export const IntroWindow = (props) => {
   return (
-    <div className="intro-window">
+    <div className={`intro-window ${props.class}`}>
       <span className="intro-span-num">{props.num}</span>
       <span className="intro-span-header">{props.header}</span>
       <img src={props.img} alt="" />
--- src/sections/Content/Intro.jsx
+++ src/sections/Content/Intro.jsx
@@ -5,18 +5,52 @@ import {
   IntroCheckDebts,
   IntroCreateGroup,
 } from "../../assets/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 export const Intro = () => {
+  const { ref, inView: elementIsVisible, entry } = useInView({ threshold: 1 });
+
+  const [currentSelected, setCurrentSelected] = useState(1);
+
+  useEffect(() => {
+    if (elementIsVisible) {
+      const interval = setInterval(() => {
+        setCurrentSelected((prevSelected) => {
+          if (prevSelected === 4) {
+            return 1;
+          } else {
+            return prevSelected + 1;
+          }
+        });
+      }, 2000);
+      return () => {
+        clearInterval(interval);
+        setCurrentSelected(1);
+      };
+    }
+  }, [elementIsVisible]);
   return (
-    <section className="intro">
+    <section ref={ref} className="intro">
       <div className="intro-container">
-        <IntroWindow header="Create group" img={IntroCreateGroup} num="1" />
+        <IntroWindow
+          header="Create group"
+          img={IntroCreateGroup}
+          num="1"
+          class={currentSelected === 1 ? "selected" : ""}
+        />
         <IntroWindow
           header="Make transactions"
           img={IntroMakeTransactions}
           num="2"
+          class={currentSelected === 2 ? "selected" : ""}
         />
-        <IntroWindow header="Check debts" img={IntroCheckDebts} num="3" />
+        <IntroWindow
+          header="Check debts"
+          img={IntroCheckDebts}
+          num="3"
+          class={currentSelected === 3 ? "selected" : ""}
+        />
       </div>
     </section>
   );
